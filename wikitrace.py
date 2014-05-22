061540b199a5a7450f8653b986d9e55b14e2f2cf
worstadmin <jpotts@JPottss-MacBook-Pro.local>
2014-05-22 16:28:12 -0400

    Slightly working version

--- wikitrace.py
+++ wikitrace.py
@@ -5,12 +5,13 @@ import sys
 from bs4 import BeautifulSoup
 
 existing_wikis = []
-prohibited = ['Wikipedia','Help','File']
+prohibited = ['Wikipedia','Help','File','Latin']
 
 def find_parent(topic):
 
     global existing_wikis    
     global prohibited 
+    output = ""
 
     article = urllib.quote(topic)
 
@@ -22,31 +23,33 @@ def find_parent(topic):
     resource.close()
     
     soup = BeautifulSoup(data)
-    output = soup.find_all("div",{"class":"mw-content-ltr"})
-    
+    divTag = soup.find_all("div",{"class":"mw-content-ltr"})
+    for tag in divTag:
+        for element in tag.find_all("p"):
+            output += str(element)    
 
-    print "\n===\n" + output + "\n===\n"
-    pattern = r'wiki/[A-Z,a-z,1-9,\),\(,_,\-]+'
+    #print "\n===\n" + output + "\n===\n"
+    pattern = r'/wiki/[A-Z,a-z,1-9,\),\(,_,\-]+'
    
     print existing_wikis
 
     for match in re.finditer(pattern,output):
         next_page = match.group().split('/')[2]
         #print "[-] Found link " + next_page        
-        print match.group()         
+        #print match.group()         
 
         if next_page in prohibited: 
             print "[-] Prohibited link found"
             continue 
 
+        if next_page == "Philosophy":
+            print "[P] Philosophy was reached"
+            sys.exit(1)
+
         if next_page not in existing_wikis:
             print next_page
             print "[-] breaking"
             break
-    
-        if next_page == "Philosophy":
-            print "Philosophy was reached"
-            sys.exit(1)
     
     existing_wikis.append(next_page)
 
