ae6dc01fe9a2df357d7ffd14c376207943f53df7
worstadmin <jpotts@JPottss-MacBook-Pro.local>
2014-05-22 17:23:01 -0400

    working a little better now - code is sloppy

--- wikitrace.py
+++ wikitrace.py
@@ -5,13 +5,14 @@ import sys
 from bs4 import BeautifulSoup
 
 existing_wikis = []
-prohibited = ['Wikipedia','Help','File','Latin']
+prohibited = ['Type','Latin_language','Wikipedia','Help','File','Latin','History','en','Help']
 
 def find_parent(topic):
 
     global existing_wikis    
     global prohibited 
     output = ""
+    next_page = ""
 
     article = urllib.quote(topic)
 
@@ -29,35 +30,47 @@ def find_parent(topic):
             output += str(element)    
 
     #print "\n===\n" + output + "\n===\n"
-    pattern = r'/wiki/[A-Z,a-z,1-9,\),\(,_,\-]+'
+    pattern = r'/wiki/[A-Z,a-z,1-9,\),\(,_,\-,\:]+'
    
-    print existing_wikis
+    #print existing_wikis
 
     for match in re.finditer(pattern,output):
         next_page = match.group().split('/')[2]
+        if next_page is None:
+            print "[-] No Wikipedia Entry for " + topic 
+            sys.exit(1)
         #print "[-] Found link " + next_page        
         #print match.group()         
 
+        if next_page is None:
+            print "[-] No Wikipedia Entry for " + topic
+            sys.exit(1)
+
         if next_page in prohibited: 
-            print "[-] Prohibited link found"
+            #print "[-] Prohibited link found"
+            continue
+
+        a = re.compile("^(Help|Wikipedia)\:.+")
+        if a.match(next_page):
             continue 
 
         if next_page == "Philosophy":
             print "[P] Philosophy was reached"
+            print existing_wikis
             sys.exit(1)
 
         if next_page not in existing_wikis:
-            print next_page
-            print "[-] breaking"
+            #print next_page
+            #print "[-] breaking"
             break
     
     existing_wikis.append(next_page)
 
-    print "[-] returning " + next_page
+    #print "[-] returning " + next_page
     return find_parent(next_page)
 
-def main():
-    print "[-] Searching term Ocean for parents..."
-    find_parent("Ocean")
+def main(input):
+    print "[-] Searching term " + input + " for parents..."
+    find_parent(input)
 
-main()    
+main(sys.argv[1])    
